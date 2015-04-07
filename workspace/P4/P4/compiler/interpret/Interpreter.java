af19a27e0801072a6d0b21b627f821db5b24ab9b
JacobNielsenn <ja_ni@live.dk>
2015-04-07 10:56:08 +0200

    no message

---
+++ workspace/P4/P4/compiler/interpret/Interpreter.java
@@ -0,0 +1,25 @@
+/* An interpreter for the simple math language we all espouse. */
+package compiler.interpret;
+ 
+import compiler.node.* ;
+import compiler.analysis.* ;
+
+import java.lang.System;
+
+public class Interpreter extends DepthFirstAdapter {
+ 
+	
+	public void defaultOut(@SuppressWarnings("unused") Node node)
+    {
+        // Do nothing
+		//System.out.println(node.getClass() + " : " + node);
+    }
+   /*
+        // Print tokens and ignores bland and EOF tokens
+    	if (!token.getClass().toString().endsWith("TBlank") && !token.getClass().toString().endsWith("EOF")){
+    		System.out.println(token.getClass() +
+                    ", state : " + state.id() +
+                    ", text : [" + token.getText() + "]");
+    	}
+   */
+} 

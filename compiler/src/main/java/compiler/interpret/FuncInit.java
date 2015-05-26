e019b3ba53c45168d628bea1b549bd307cf1bf84
Kenneth Husum Stick <super-stick@hotmail.com>
2015-05-26 14:57:30 +0200

    symbol table bliver ikke brugt i function chercker længer

--- compiler/src/main/java/compiler/interpret/FuncInit.java
+++ compiler/src/main/java/compiler/interpret/FuncInit.java
@@ -3,6 +3,7 @@ package compiler.interpret;
 import compiler.analysis.DepthFirstAdapter;
 import compiler.node.AFuncDecl;
 import compiler.node.AObjectDecl;
+import compiler.node.TIdentifier;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -34,6 +35,13 @@ public class FuncInit extends DepthFirstAdapter {
     {
         if (!InObj)
         {
+            TIdentifier ident = node.getIdentifier();
+            String key = ident.toString().toUpperCase().trim();
+            if (symTable.FuncPrevDeclared(key))
+            {
+                System.out.println("Function already defined: " + ident);
+                System.exit(0);
+            }
             List<TypeExpression> typeExpressions = new LinkedList<>();
             node.apply(new FunctionChecker(symTable, typeExpressions));
             symTable.AddFunction(node.getIdentifier().toString().toUpperCase().trim(), typeExpressions);
